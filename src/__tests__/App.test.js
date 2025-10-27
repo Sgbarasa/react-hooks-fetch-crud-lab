9980ae5bced402cf6319d59bbe3fdfd6a08fc6a0
stanley <sgbarasa@gmail.com>
2025-10-27 10:55:37 +0300

    final project

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -6,7 +6,7 @@ import {
   screen,
   waitForElementToBeRemoved,
 } from "@testing-library/react";
-import "@testing-library/jest-dom";
+//import "@testing-library/jest-dom";
 import { server } from "../mocks/server";
 
 import App from "../components/App";
